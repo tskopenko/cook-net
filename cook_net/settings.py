d44758906c5e2ae09e30da8f0fbbbfa1532b656e
Tymofii Skopenko <youmustdeth@gmail.com>
2024-03-19 17:46:11 +0200

    update settings & requirements.txt

--- cook_net/settings.py
+++ cook_net/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h$k-fe!qpi8=u&sy8j5j@i(j1)2w5=rt11nzsmz4m^41%_xyrq'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-h$k-fe!qpi8=u&sy8j5j@i(j1)2w5=rt11nzsmz4m^41%_xyrq")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
 # Application definition
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'cook_net',
     'core',
 ]
