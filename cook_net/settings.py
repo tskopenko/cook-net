9433580635f9601a517a17a72e497e8d2feea704
Tymofii Skopenko <youmustdeth@gmail.com>
2024-03-20 17:04:22 +0200

    update the middleware & host allowances

--- cook_net/settings.py
+++ cook_net/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-h$k-fe!qpi8=u&
 # DEBUG = False
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "cook-net.onrender.com"]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhitenoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
